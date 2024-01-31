33885cfe864490a50c6f6ef31a76c9ecab488c23
gourteacher <ouaazs@algonquincollege.com>
2024-01-31 17:01:15 -0500

    Activity Result Launcher

--- app/src/main/java/com/college/examples/FirstActivity.java
+++ app/src/main/java/com/college/examples/FirstActivity.java
@@ -1,50 +1,30 @@
 package com.college.examples;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FirstActivity extends AppCompatActivity {
 
-    public final static String TAG ="FirstActivity";
-
-    public final static String PREFERENCES_FILE = "MyData";
+    private final static String TAG ="FirstActivity";
 
     @Override     //first called
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //calls parent onCreate()
+        setContentView( R.layout.activity_main ); //loads XML on screen
 
-        Log.i(TAG, "In onCreate, creating the objects");
-        setContentView( R.layout.activity_first); //loads XML on screen
-
-        SharedPreferences prefs = getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
-        //Read preferences
-        String previous = prefs.getString("ReserveName", "Default Value");
-        TextView edit = findViewById(R.id.reserved_name);
-        edit.setText(previous);
-
-        //SharedPreferences
-        Button btn3 = findViewById(R.id.shared_preferences);
-        btn3.setOnClickListener( (  click ) ->
+        Button btn = findViewById(R.id.start_button);
+        btn.setOnClickListener(  (  click ) ->
         {
-            EditText userText = findViewById(R.id.user_input);
-            String userTyped = userText.getText().toString();
-            Intent nextPage = new Intent(FirstActivity.this,   SharedPrefsActivity.class  );
-
-            nextPage.putExtra("USERINPUT", userTyped);
-            nextPage.putExtra("MONTH", 10);
-            nextPage.putExtra("OTHER INFO", 3.14);
-
+            //Where you are     //where we're going
+            Intent nextPage = new Intent(FirstActivity.this,   SecondActivity.class  );
             //Make the transition:
-            startActivity(    nextPage  );
-        });
+            startActivity(nextPage);
+        } ); //OnCLickListener goes in here
+
     }
 
     @Override //screen is visible but not responding
