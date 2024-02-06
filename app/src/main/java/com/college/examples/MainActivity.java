814b57760ccd3ef45f879f1b0d7978755043b8f4
gourteacher <ouaazs@algonquincollege.com>
2024-02-05 23:07:39 -0500

    default app back

--- app/src/main/java/com/college/examples/MainActivity.java
+++ app/src/main/java/com/college/examples/MainActivity.java
@@ -19,58 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(binding.getRoot());
 
-        binding.loginButton.setOnClickListener(click -> {
-            String userInput = binding.passwordText.getText().toString();
-            if(containsUpperAndLowerCase(userInput)){
-                binding.responseText.setText(R.string.upper_lower_case_msg);
-            }
-            else if(containsNumber(userInput)){
-                binding.responseText.setText(R.string.number_msg);
-            }
-            else binding.responseText.setText(R.string.not_complex_msg);
-        });
      }
 
-    /** This function checks str and returns true if str
-     * has an upper and lower case letter in it
-     * @param str - an unused parameter
-     *
-     * @return Returns true if str has upper and lower case, otherwise false
-     */
-    boolean containsUpperAndLowerCase( String str){
-        boolean foundUpper = false;
-        boolean foundLower = false;
 
-        for(int i = 0; i < str.length(); i++)
-        {
-            //get each character in the string:
-            char c = str.charAt(i);
-            if(Character.isLowerCase(c))
-                foundLower = true;
-            else if(Character.isUpperCase(c))
-                foundUpper = true;
-        }
-        //after looked at every character
-        return foundLower && foundUpper;
-    }
-
-    /** This function checks str and returns true if str
-     * has a number in it
-     * @param str - an unused parameter
-     *
-     * @return Returns true if str has a digit, otherwise false
-     */
-    boolean containsNumber( String str){
-        boolean foundNumber = false;
-
-        for(int i = 0; i < str.length(); i++)
-        {
-            //get each character in the string:
-            char c = str.charAt(i);
-            if(Character.isDigit(c))
-                foundNumber = true;
-        }
-        //after looked at every character
-        return foundNumber;
-    }
 }
